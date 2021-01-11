2174d35efc61808e37eaa434485afc7ef9bcd20d
Ilya Shablovsky <ishablovsky@shift4.com>
2021-01-11 15:37:21 +0300

    three-js: builderResolver: remove redundant check

--- ThreeJS/src/builderResolver.js
+++ ThreeJS/src/builderResolver.js
@@ -1,8 +1,7 @@
 import componentToBuilderMap from './mappers/componentToBuilder';
-import componentTypes from './enums/componentTypes';
 
 function resolve({ components }) {
-	const [component] = components.filter((item) => item.type != componentTypes.TRANSFORM);
+	const [component] = components;
 	const builder = componentToBuilderMap[component.type];
 
 	return builder;
